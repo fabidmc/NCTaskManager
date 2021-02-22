daf023f20620c6c5acffc2fb194b61a671cb89b4
fabidmc <fabiola.dlmorac@comunidad.unam.mx>
2021-02-21 20:33:54 -0600

    Modified

--- src/main/java/mx/edu/j2se/DeLaMora/tasks/Task.java
+++ src/main/java/mx/edu/j2se/DeLaMora/tasks/Task.java
@@ -5,16 +5,15 @@ public class Task {
     // VARIABLES AND THE TYPE
     String title;
     int time, startTime, endTime, interval, current;
-    boolean active, repeated = true;
+    boolean active, repeated;
 
-
-    // Constructor for non-repeative task
+    // Constructor for non-repetitive task
     public Task(String title, int time) {
         this.title = title;
         this.time = time;
     }
 
-    // Constructor for a  repeatitive task
+    // Constructor for a  repetitive task
     public Task(String title, int startTime, int endTime, int interval) {
         this.title = title;
         this.startTime = startTime;
@@ -22,72 +21,95 @@ public class Task {
         this.interval = interval;
     }
 
-    /** return string
-     *
-     */
 
     // METHODS
+
     // Method for reading and setting the task name
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
+
         this.title = title;
     }
 
+    //Method for reading and setting the task status
     public boolean isActive(){
-        boolean active = this.active;
         return active;
     }
 
     public void setActive(boolean active){
+
         this.active = active;
     }
 
+
+    //METHODS FOR NON-REPETITIVE TASKS
+
+    //Method for changing execution time for non-repetitive tasks
     public int getTime() {
-        if (repeated == true) {
+        if (this.interval == 0) {
             return time;
         }
-        return 0;
+        return startTime;
     }
 
     public void setTime(int time) {
+        if(this.interval != 0){
+            this.startTime = 0;
+            this.endTime = 0;
+            this.interval = 0;
+        }
         this.time = time;
     }
 
+    // METHODS FOR REPETITIVE TASKS
+    //Method for reading and changing execution time for repetitive tasks
     public int getStartTime() {
-        if(repeated == false){
+        if(this.interval != 0){
             return startTime;
         }
-        return 0;
+        return time;
     }
 
     public void setStartTime(int startTime) {
         this.startTime = startTime;
     }
 
+
     public int getEndTime() {
-        return endTime;
+        if(this.interval != 0){
+            return endTime;
+        }
+
+        return time;
     }
 
     public void setEndTime(int endTime) {
+
         this.endTime = endTime;
     }
 
-    public int getInterval() {
-        return interval;
+    public int getRepeatInterval(){
+        if(this.interval != 0){
+            return interval;
+        }
+        return 0;
     }
 
-    public void setInterval(int interval) {
-        this.interval = interval;
+
+    public void setTime(int startTime, int endTime, int interval) {
+
+        if (this.interval == 0) {
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.interval = interval;
+        }
+
+
     }
 
-    public boolean isRepeated() {
-        return repeated;
-    }
-
-        
 
 
 }
