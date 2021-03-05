f362056944207d36e33670e9ea74596ce9f60645
fabidmc <fabiola.dlmorac@comunidad.unam.mx>
2021-03-04 19:33:40 -0600

    EVALUACION 1

---
+++ src/main/java/mx/edu/j2se/DeLaMora/evaluation/Circle.java
@@ -0,0 +1,38 @@
+package mx.edu.j2se.DeLaMora.evaluation;
+
+public class Circle {
+
+    private int radius;
+
+    //Constructor
+    public Circle(){
+       radius = 1;
+
+    }
+
+    public Circle(int radius){
+
+        this.radius = radius;
+
+        if(radius < 0){
+            throw new IllegalArgumentException("Not valid negative arguments !!");
+
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+
+        if(this.radius < 0){
+            throw new IllegalArgumentException("This is not possible...try again");
+        }
+    }
+
+    public boolean getArea(int circle){
+        return Math.PI * radius * radius;
+    }
+}
---
+++ src/main/java/mx/edu/j2se/DeLaMora/evaluation/evaluation1.java
@@ -0,0 +1,35 @@
+package mx.edu.j2se.DeLaMora.evaluation;
+
+public class evaluation1 {
+
+
+    public static <circles> int main(String[] args) {
+
+        int i;
+        Circle circ = new Circle(-2);
+
+
+        int[] circles = new int[3];
+        circles[0] = 3;
+        circles[1] = 4;
+        circles[2] = 5;
+
+
+        public void biggestCircle(circles[]) {
+
+        for (i = 0; i < circles.length; i++) {
+
+           // if (circ.getArea(circles[i])) {
+            //    return i;
+           // }
+
+
+        }
+    }
+    }
+
+
+
+
+
+
