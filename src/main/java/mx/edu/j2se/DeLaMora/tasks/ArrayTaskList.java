07f7713a1d5fbe962b731f8e2c54ac0851004742
fabidmc <fabiola.dlmorac@comunidad.unam.mx>
2021-03-09 18:01:47 -0600

    PRACTICA 2 MODIFICADA

---
+++ src/main/java/mx/edu/j2se/DeLaMora/tasks/ArrayTaskList.java
@@ -0,0 +1,69 @@
+package mx.edu.j2se.DeLaMora.tasks;
+
+ import java.util.*;
+
+public class ArrayTaskList {
+
+    public int index = 0;
+
+    Task[] arrayList = new Task[0];
+    final int N = arrayList.length;
+
+    // METHOD FOR ADDING A NEW TASK
+
+    public void add(Task task) {
+
+        Task[] newarr = Arrays.copyOf(arrayList, N + 1);
+        newarr[0] = task;
+        arrayList[N - 1] = newarr[0];
+
+    }
+
+
+    // METHOD FOR REMOVING TASKS THAT ARE DUPLICATES
+
+    public boolean remove(Task task) {
+
+        int noUnique = arrayList.length;
+        for (int i = 0; i < noUnique; i++) {
+            for (int j = i + 1; j < noUnique; j++) {
+                if (arrayList[i] == arrayList[j]) {
+                    arrayList[j] = arrayList[noUnique - 1];
+                    noUnique--;
+                    j--;
+
+                }
+            }
+            Task[] array1 = Arrays.copyOf(arrayList, noUnique);
+
+            if(noUnique>index){
+                return true;
+        }
+
+
+        }
+    return false;
+    }
+
+
+    public int size() {
+        return arrayList.length;
+    }
+
+    public Task getTask(int index) {
+     try {
+         if (index < 0 || index > this.index) {
+             throw new IndexOutOfBoundsException(" ERROR!!! ");
+         }
+     }
+     catch (IndexOutOfBoundsException d){
+        }
+            return arrayList[index];
+
+    }
+
+
+}
+
+
+
