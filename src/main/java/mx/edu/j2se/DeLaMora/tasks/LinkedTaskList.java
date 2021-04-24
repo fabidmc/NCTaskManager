e5bdce022a76cb3b2a82faa476ccf5513bdfd731
fabidmc <fabiola.dlmorac@comunidad.unam.mx>
2021-04-23 23:19:48 -0500

    PRACTICE 3

--- src/main/java/mx/edu/j2se/DeLaMora/tasks/LinkedTaskList.java
+++ src/main/java/mx/edu/j2se/DeLaMora/tasks/LinkedTaskList.java
@@ -1,93 +1,48 @@
 package mx.edu.j2se.DeLaMora.tasks;
 
-import org.w3c.dom.*;
-
-import javax.xml.soap.Node;
 import java.io.*;
-import javax.xml.soap.SOAPElement;
-import javax.xml.soap.SOAPException;
-import java.util.LinkedList;
-
-
+import java.time.format.TextStyle;
 
 public class LinkedTaskList extends AbstractTaskList {
 
-    public Node head;
-    public int length = 0;
-    public Node newnode;
+    public Node head ;
+    public int size = 0;
 
-
-    /* Linked list Node*/
     public class Node{
         Task data;
         Node next;
-    }
 
-
-    //METHOD FOR ADDING TASKS;
-    int index = 0;
-
-    public void add(Task task) {
-
-        Node node = new Node(task);
-
-        if(head.equals(null)) {
-            head = node;
-        } else{
-            Node newnode = head;
-        while (newnode.next != null){
-            newnode = newnode.getNext();
-        }
-        iterator.setNext(node);
-        length++;
-    }
-
-    // METHOD FOR REMOVING DUPLICATE TASKS
-    public boolean remove(Object data) {
-
-        if (head.getNext() == null){
-            return false;
+        Node(Task n){
+            data = n;
+            next = null;
         }
 
-        Node iterator = head;
+        //METHOD TO INSERT A NEW ELEMENT
 
-        while(iterator.getNext() != null){
+        public void add(Task task){
+            Node newnode = new Node();
+            newnode.data = task;
+            newnode.next = null;
 
-            if (iterator.getNext().getData().equals(data)){
-                iterator.setNext(iterator.getNext().getNext());
-                length--;
-                return true;
-            }
+            if(head == null){
+                head = newnode;}
+                else{
+                    Node t = head;
+                    while(t.next != null){
+                        t = t.next;
 
-            iterator = iterator.getNext();
-        }
+                }
+                    t.next = newnode;
 
-        return false;
-    }
-
-
-    // METHOD FOR SIZE OF LINKEDLIST
-            public int size() {
-                return newarr.size();
-            }
-
-            //METHOD FOR GETTING TASK
-
-
-            @Override
-            public org.w3c.dom.Node removeChild(org.w3c.dom.Node oldChild) throws DOMException {
-                return null;
-            }
-
-            @Override
-            public org.w3c.dom.Node appendChild(org.w3c.dom.Node newChild) throws DOMException {
-                return null;
             }
 
 
-        public Task next(){
-            index++;
-            return getTask(index-1);
         }
+
     }
-    }
+}
+
+
+
+
+
